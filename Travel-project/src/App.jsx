a03d856bc9926222c9231c8aa71f4053a166de2f
afika <afikashwashwa@gmail.com>
2023-11-13 18:46:29 +0200

    Initialised Travel app using React JS

---
+++ Travel-project/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+// import HomeImage from './assets/images/ride.jpg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='main'>
+        <div>
+
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default App
